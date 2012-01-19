f4d2c7bab62908f1b832f157f25b2d7ead2bfd51
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-01-19 15:16:03 +0100

    proper url

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Thanks to http://vimcasts.org/episodes/synchronizing-plugins-with-git-submodules-and-pathogen/
 cd ~
-git clone gitgithub.com/tdi/vimrc.git ~/.vim
+git clone git://github.com/tdi/vimrc.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
 cd ~/.vim
 git submodule init
 git submodule update
