527636c154185917c11aa5f085922130701fca3f
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2015-05-15 14:44:03 +0200

    Cleanup

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Thanks to http://vimcasts.org/episodes/synchronizing-plugins-with-git-submodules-and-pathogen/
-cd ~
-git clone git://github.com/tdi/vimrc.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim
-git submodule init
-git submodule update
